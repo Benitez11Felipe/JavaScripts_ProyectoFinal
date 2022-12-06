8c7a9045819d63c46ad75e3d3ca25eead214bbbe
Benitez11Felipe <116870493+Benitez11Felipe@users.noreply.github.com>
2022-12-06 11:32:43 -0300

    Update script.js

--- pages/mru/js/script.js
+++ pages/mru/js/script.js
@@ -1,48 +1,54 @@
-let problema = prompt("Ingrese el problema de MRU que desea resolver. por ejemplo: distancia");
-
-if ((problema == "distancia")||(problema == "Distancia")){
-    let velocidad = parseFloat (prompt("Ingresar la velocidad en Km/h"));
-    let tiempo = parseFloat (prompt("Ingresar el tiempo en Hs"));
+function distancia (velocidad , tiempo){
     let distancia = velocidad * tiempo;
     let distanciaEnMetros = distancia * 1000;
     alert("La distancia que recorrio o recorrerá es de "+distancia+"Km o tambien "+distanciaEnMetros+"m");
 }
-else if ((problema == "tiempo")||(problema == "Tiempo")){
-    let distancia = parseFloat (prompt("Ingrese la distancia en Km"));
-    let velocidad = parseFloat (prompt("Ingresar la velocidad en Km/h"));
+
+function tiempo (distancia , velocidad){
     let tiempo = distancia / velocidad;
     let tiempoEnSegundos = tiempo * 3600;
     alert("El tiempo que tardo es de "+tiempo+"Hs o "+tiempoEnSegundos+"seg");
 }
-else if ((problema == "velocidad")||(problema == "Velocidad")){
-    let distancia = parseFloat (prompt("Ingrese la distancia en Km"));
-    let tiempo = parseFloat (prompt("Ingresar el tiempo en hs"));
+
+function velocidad (distancia , tiempo){
     let velocidad = distancia / tiempo;
     let velocidadEnMetrosPorSegundos = velocidad / 3.6;
     alert("La velocidad es de "+velocidad+"Km/h o "+velocidadEnMetrosPorSegundos+"m/s");
 }
+
+let problema = prompt("Ingrese el problema de MRU que desea resolver. por ejemplo: Distancia - Velocidad - Tiempo");
+
+if ((problema == "distancia")||(problema == "Distancia")){
+    let velocidad = parseFloat (prompt("Ingresar la velocidad en Km/h"));
+    let tiempo = parseFloat (prompt("Ingresar el tiempo en Hs"));
+    distancia (velocidad , tiempo);
+}
+else if ((problema == "tiempo")||(problema == "Tiempo")){
+    let distancia = parseFloat (prompt("Ingrese la distancia en Km"));
+    let velocidad = parseFloat (prompt("Ingresar la velocidad en Km/h"));
+    tiempo (distancia , velocidad);
+}
+else if ((problema == "velocidad")||(problema == "Velocidad")){
+    let distancia = parseFloat (prompt("Ingrese la distancia en Km"));
+    let tiempo = parseFloat (prompt("Ingresar el tiempo en hs"));
+    velocidad (distancia , tiempo);
+}
 else{
     alert("ERROR: Ingresar bien el nombre de su operacion.")
-    let problema = prompt("Ingrese el problema de MRU que desea resolver. por ejemplo: distancia");
+    let problema = prompt("Ingrese el problema de MRU que desea resolver. por ejemplo: Distancia - Velocidad - Tiempo");
     if ((problema == "distancia")||(problema == "Distancia")){
         let velocidad = parseFloat (prompt("Ingresar la velocidad en Km/h"));
         let tiempo = parseFloat (prompt("Ingresar el tiempo en Hs"));
-        let distancia = velocidad * tiempo;
-        let distanciaEnMetros = distancia * 1000;
-        alert("La distancia que recorrio o recorrerá es de "+distancia+"Km o tambien "+distanciaEnMetros+"m");
+        distancia (velocidad , tiempo);
     }
     else if ((problema == "tiempo")||(problema == "Tiempo")){
         let distancia = parseFloat (prompt("Ingrese la distancia en Km"));
         let velocidad = parseFloat (prompt("Ingresar la velocidad en Km/h"));
-        let tiempo = distancia / velocidad;
-        let tiempoEnSegundos = tiempo * 3600;
-        alert("El tiempo que tardo es de "+tiempo+"Hs o "+tiempoEnSegundos+"seg");
+        tiempo (distancia , velocidad);
     }
     else if ((problema == "velocidad")||(problema == "Velocidad")){
         let distancia = parseFloat (prompt("Ingrese la distancia en Km"));
         let tiempo = parseFloat (prompt("Ingresar el tiempo en hs"));
-        let velocidad = distancia / tiempo;
-        let velocidadEnMetrosPorSegundos = velocidad / 3.6;
-        alert("La velocidad es de "+velocidad+"Km/h o "+velocidadEnMetrosPorSegundos+"m/s");
+        velocidad (distancia , tiempo);
     }
 }
