2871683a1261727cfd3a5e7ada4bfb25d7625561
Benitez11Felipe <116870493+Benitez11Felipe@users.noreply.github.com>
2022-12-13 05:41:03 -0300

    Update script.js

--- pages/mru/js/script.js
+++ pages/mru/js/script.js
@@ -1,54 +1,59 @@
-function distancia (velocidad , tiempo){
-    let distancia = velocidad * tiempo;
-    let distanciaEnMetros = distancia * 1000;
-    alert("La distancia que recorrio o recorrerá es de "+distancia+"Km o tambien "+distanciaEnMetros+"m");
+function calculo (num1, num2, operacion){
+    let resultadoMru = 0
+    switch (operacion) {
+        case "espacio":
+            resultadoMru = num1 * num2;
+            distanciaEnMetros = resultadoMru * 1000;
+            alert("La distancia que recorrio o recorrerá es de "+resultadoMru+"Km o tambien "+distanciaEnMetros+"m");
+            break;
+        case "tiempo":
+            resultadoMru = num1 / num2;
+            tiempoEnSegundos = resultadoMru * 3600;
+            alert("El tiempo que tardo es de "+resultadoMru+"Hs o "+tiempoEnSegundos+"seg");
+            break;
+        case "velocidad":
+            resultadoMru = num1 / num2;
+            velocidadEnMetrosPorSegundos = resultadoMru / 3.6;
+            alert("La velocidad es de "+velocidad+"Km/h o "+velocidadEnMetrosPorSegundos+"m/s");
+            break;
+        default:
+            break;
+    }
 }
 
-function tiempo (distancia , velocidad){
-    let tiempo = distancia / velocidad;
-    let tiempoEnSegundos = tiempo * 3600;
-    alert("El tiempo que tardo es de "+tiempo+"Hs o "+tiempoEnSegundos+"seg");
-}
+let problema = prompt("Ingrese el problema de MRU que desea resolver. por ejemplo: Espacio - Velocidad - Tiempo");
 
-function velocidad (distancia , tiempo){
-    let velocidad = distancia / tiempo;
-    let velocidadEnMetrosPorSegundos = velocidad / 3.6;
-    alert("La velocidad es de "+velocidad+"Km/h o "+velocidadEnMetrosPorSegundos+"m/s");
+if ((problema == "espacio")||(problema == "Espacio")||(problema == "ESPACIO")){
+    let num1 = parseFloat (prompt("Ingresar la velocidad en Km/h"));
+    let num2 = parseFloat (prompt("Ingresar el tiempo en Hs"));
+    calculo (num1, num2, "espacio");
 }
-
-let problema = prompt("Ingrese el problema de MRU que desea resolver. por ejemplo: Distancia - Velocidad - Tiempo");
-
-if ((problema == "distancia")||(problema == "Distancia")){
-    let velocidad = parseFloat (prompt("Ingresar la velocidad en Km/h"));
-    let tiempo = parseFloat (prompt("Ingresar el tiempo en Hs"));
-    distancia (velocidad , tiempo);
+else if ((problema == "tiempo")||(problema == "Tiempo")|(problema == "TIEMPO")){
+    let num1 = parseFloat (prompt("Ingrese la distancia en Km"));
+    let num2 = parseFloat (prompt("Ingresar la velocidad en Km/h"));
+    tiempo (num1, num2, "tiempo");
 }
-else if ((problema == "tiempo")||(problema == "Tiempo")){
-    let distancia = parseFloat (prompt("Ingrese la distancia en Km"));
-    let velocidad = parseFloat (prompt("Ingresar la velocidad en Km/h"));
-    tiempo (distancia , velocidad);
-}
-else if ((problema == "velocidad")||(problema == "Velocidad")){
-    let distancia = parseFloat (prompt("Ingrese la distancia en Km"));
-    let tiempo = parseFloat (prompt("Ingresar el tiempo en hs"));
-    velocidad (distancia , tiempo);
+else if ((problema == "velocidad")||(problema == "Velocidad")||(problema == "VELOCIDAD")){
+    let num1 = parseFloat (prompt("Ingrese la distancia en Km"));
+    let num2 = parseFloat (prompt("Ingresar el tiempo en hs"));
+    velocidad (num1, num2, "velocidad");
 }
 else{
     alert("ERROR: Ingresar bien el nombre de su operacion.")
-    let problema = prompt("Ingrese el problema de MRU que desea resolver. por ejemplo: Distancia - Velocidad - Tiempo");
-    if ((problema == "distancia")||(problema == "Distancia")){
-        let velocidad = parseFloat (prompt("Ingresar la velocidad en Km/h"));
-        let tiempo = parseFloat (prompt("Ingresar el tiempo en Hs"));
-        distancia (velocidad , tiempo);
+    let problema = prompt("Ingrese el problema de MRU que desea resolver. por ejemplo: Espacio - Velocidad - Tiempo");
+    if ((problema == "espacio")||(problema == "Espacio")||(problema == "ESPACIO")){
+        let num1 = parseFloat (prompt("Ingresar la velocidad en Km/h"));
+        let num2 = parseFloat (prompt("Ingresar el tiempo en Hs"));
+        calculo (num1, num2, "espacio");
     }
-    else if ((problema == "tiempo")||(problema == "Tiempo")){
-        let distancia = parseFloat (prompt("Ingrese la distancia en Km"));
-        let velocidad = parseFloat (prompt("Ingresar la velocidad en Km/h"));
-        tiempo (distancia , velocidad);
+    else if ((problema == "tiempo")||(problema == "Tiempo")|(problema == "TIEMPO")){
+        let num1 = parseFloat (prompt("Ingrese la distancia en Km"));
+        let num2 = parseFloat (prompt("Ingresar la velocidad en Km/h"));
+        tiempo (num1, num2, "tiempo");
     }
-    else if ((problema == "velocidad")||(problema == "Velocidad")){
-        let distancia = parseFloat (prompt("Ingrese la distancia en Km"));
-        let tiempo = parseFloat (prompt("Ingresar el tiempo en hs"));
-        velocidad (distancia , tiempo);
+    else if ((problema == "velocidad")||(problema == "Velocidad")||(problema == "VELOCIDAD")){
+        let num1 = parseFloat (prompt("Ingrese la distancia en Km"));
+        let num2 = parseFloat (prompt("Ingresar el tiempo en hs"));
+        velocidad (num1, num2, "velocidad");
     }
 }
