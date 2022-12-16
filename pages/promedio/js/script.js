9e904534b42cbaea9ce7003f9dc43578df2938bd
Benitez11Felipe <116870493+Benitez11Felipe@users.noreply.github.com>
2022-12-16 12:10:10 -0300

    Add files via upload

---
+++ pages/promedio/js/script.js
@@ -0,0 +1,13 @@
+let contadorDeNotas = 0;
+let sumadorDeNotas = 0;
+
+let nota = parseInt(prompt("ingresar nota: (para finalizar ingrese -1)"));
+
+while (nota != -1){
+    contadorDeNotas = contadorDeNotas + 1;
+    sumadorDeNotas = sumadorDeNotas + nota;
+    nota = parseInt(prompt("ingresar nota: (para finalizar ingrese -1)"));
+}
+
+let promedio = sumadorDeNotas / contadorDeNotas;
+alert ("La suma de las notas es de "+sumadorDeNotas+", ingreso "+contadorDeNotas+" notas, eso quiere decir que su promedio es de "+promedio);
